b7c481db906c69c4864c50c0cb54acf313723785
Rakesh Chotalia <Rakesh.Chotalia@imgtec.com>
2020-01-16 09:13:27 +0000

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,6 +25,11 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +42,7 @@ namespace API
 
             // app.UseHttpsRedirection(); // redirect HTTP requests to HTTPS
 
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseAuthorization();
